c7683486a0445761345766c35b86a82ef6e9f546
Omkar <greed8039@gmail.com>
2019-07-26 07:26:39 +0530

    Nqueen

--- backtracking/Nqueen.cpp
+++ backtracking/Nqueen.cpp
@@ -1,69 +1,90 @@
-#include<bits/stdc++.h>
-#define deb(x) cout<<#x<< ": "<<x<<"\n";
-#define debug(i,j) cout<<"("<<#i<<","<<#j<<")"<<":"<<i<<","<<j<<"\n";
+#include<iostream>
+#define QUEEN 1
 using namespace std;
-bool isSafe(vector<vector<int>> board,int i,int j){
-	for(int row=0;row<i;row++){
-		if(board[row][j]==1)
-			return false;
-	}
-	int x,y;
-	x=i;
-	y=j;
-	while(x>=0&&y>=0){
-		if(board[x][y]==1)
-			return false;
-		x--;
-		y--;
-	}
-	int k,l;
-	k=i;
-	l=j;
-	while(k>=0&&j<board[0].size()){
-		if(board[k][l]==1)
-			return false;
-		i--;
-		j++;
-	}
-	for(int row=board.size()-1;row>=0;row--){
-		if(board[row][j]==1)
-			return false;
-	}
-
-	return true;
-}
-void printSolution(std::vector<vector<int>> board)
-{
-	for(int i=0;i<board.size();i++){
-		for(int j=0;j<board[0].size();j++){
-			cout<<board[i][j]<<" ";
+int n_queen;
+int board_size;
+void printSolution(int **chess_board){
+	cout<<"Printing solution"<<"\n";
+	for(int i=0;i<board_size;i++){
+		for(int j=0;j<board_size;j++){
+			if(chess_board[i][j]==1)
+                cout<<"Q"<<" ";
+            else
+                cout<<"-"<<" ";
 		}
 		cout<<"\n";
 	}
-	
 }
-void solve(vector<vector<int>> &board,int i,int n){
-	if(i==n){
-		printSolution(board);
-		return;
-	}
+bool isSafe(int row,int col,int **chess_board){
+int i,j;
+   
+    for( i=0;i<board_size;i++)
+      if(chess_board[i][col]==1)
+        return false;
 
-	for(int row=0;row<n;row++){
-		if(isSafe(board,row,i)&& board[row][n]!=1){
-			board[row][i]=1;
-			solve(board,i+1,n);
-			board[row][i]=0;
-		}
-	}
+    for(int i=0;i<board_size;i++)
+        if(chess_board[row][i]==1)
+            return false;
+  
+    i=row-1;
+    j=col+1;
+    while(i>=0&&j<board_size)
+    {
+        if(chess_board[i][j]==1)
+            return false;
+        i--;
+        j++;
+    }
+    i=row-1;
+    j=col-1;
+    while(i>=0&&j>=0)
+    {
+        if(chess_board[i][j]==1)
+            return false;
+        i--;
+        j--;
+    }
+    return true;
+}
+void solveNQueen(int **chess_board,int i,int n_queen){
+	 if(i>=n_queen)
+    {
+        printSolution(chess_board);
+        return ;
+    }
+
+    for(int col=0;col<board_size;col++)
+    {
+        if(isSafe(i,col,chess_board))
+        {
+            chess_board[i][col]=1;
+
+          solveNQueen(chess_board,i+1,n_queen);
+           
+
+            chess_board[i][col]=0;
+        }
+    }
+return;
 }
 int main(){
 	int t;
 	cin>>t;
 	while(t--){
-		int n;
-		cin>>n;
-		vector<vector<int>> board(n,vector<int>(n,0));
-		int start=0;
-		solve(board,start,n);
+		
+		cin>>n_queen;
+		cin>>board_size;
+		int** chess_board=new int*[board_size];
+		for(int i=0;i<board_size;i++){
+			chess_board[i]=new int[board_size];
+		}
+		for(int i=0;i<board_size;i++){
+			for(int j=0;j<board_size;j++)
+				chess_board[i][j]=0;
+		}
+		
+		solveNQueen(chess_board,0,n_queen);
+
+
 	}
 }
